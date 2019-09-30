018206074e6b61b2b716c3a372a487422c6d50dd
Lukas Freimonas <46491687+LukasFrm@users.noreply.github.com>
2019-09-30 23:22:09 +0300

    Update precache-manifest.f9f255733c0e8088ac608a66356415d8.js

--- precache-manifest.f9f255733c0e8088ac608a66356415d8.js
+++ precache-manifest.f9f255733c0e8088ac608a66356415d8.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
-  {
-    "revision": "e5a9171df8cd2061bf984006650beb65",
-    "url": "/index.html"
-  },
-  {
-    "revision": "c4eaef7e1bef1dfa0154",
-    "url": "/static/css/2.0624914a.chunk.css"
-  },
-  {
-    "revision": "76c40b540c8039d70bab",
-    "url": "/static/css/main.e60e407b.chunk.css"
-  },
-  {
-    "revision": "c4eaef7e1bef1dfa0154",
-    "url": "/static/js/2.510c6cb6.chunk.js"
-  },
-  {
-    "revision": "76c40b540c8039d70bab",
-    "url": "/static/js/main.c436a199.chunk.js"
-  },
-  {
-    "revision": "da14533cc8320ae50019",
-    "url": "/static/js/runtime-main.fb3cd7a2.js"
-  }
-]);
+    {
+      "revision": "e5a9171df8cd2061bf984006650beb65",
+      "url": "/index.html"
+    },
+    {
+      "revision": "c4eaef7e1bef1dfa0154",
+      "url": "news_app_build/static/css/2.0624914a.chunk.css"
+    },
+    {
+      "revision": "76c40b540c8039d70bab",
+      "url": "news_app_build/static/css/main.e60e407b.chunk.css"
+    },
+    {
+      "revision": "c4eaef7e1bef1dfa0154",
+      "url": "news_app_build/static/js/2.510c6cb6.chunk.js"
+    },
+    {
+      "revision": "76c40b540c8039d70bab",
+      "url": "news_app_build/static/js/main.c436a199.chunk.js"
+    },
+    {
+      "revision": "da14533cc8320ae50019",
+      "url": "news_app_build/static/js/runtime-main.fb3cd7a2.js"
+    }
+  ]);
